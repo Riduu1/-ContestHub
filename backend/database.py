ae954fd876ad8434783d1dd74d0e07355b1ab3ac
Riduan Islam <riduanislam104799@gmail.com>
2026-09-01 01:52:36 +0600

    Use environment database URL

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "postgresql://postgres:RmsjirittL@localhost:5432/contest_hub"
-
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-
 
 SessionLocal = sessionmaker(
     bind=engine,
